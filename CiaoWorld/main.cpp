6f1b1593c032f598b98ec62b8352202aa0269f83
janklasen <33368027+janklasen@users.noreply.github.com>
2017-11-06 22:17:24 +0100

    Revert "Added Hilo game"
    This reverts commit fdfd75b51ab1cc6f87ff54d3c97c0e0d86949fe0.

--- CiaoWorld/main.cpp
+++ CiaoWorld/main.cpp
@@ -32,7 +32,7 @@
 
 
 //functions
-void PrintEvenNumbersTo20()
+void printEvenNumbersTo20()
 {
 	for (short count = 0; count <= 20; count+=2)
 	{
@@ -41,36 +41,35 @@ void PrintEvenNumbersTo20()
 		
 	}
 }
-int SumTo(int value)
+int sumTo(int value)
 {
 	int result{ 0 };
 
-	for (; value > 0; --value)
+	for (; value > 0; value--)
 		result += value;
 
 	return result;
 }
-int BasicRandomNumberGenerator()
+int PRNG()
 {
-	static unsigned int s_seed = 5233;
+	static unsigned int seed = 5233;
 
-	s_seed = s_seed * 9898251 + 2152108;
-	return s_seed % 32768;
+	seed = seed * 9898251 + 2152108;
+	return seed % 32768;
 }
-int GetRandomNumber(int min, int max)
+int getRandomNumber(int min, int max)
 {
 	//get random number between  min and max
 	//makes use of srand() being defined as follows:
 	//srand(static_cast<unsigned int>(time(0)));
-	
-	
-	static const double s_fraction = (1.0 / static_cast<double>(RAND_MAX));
+
+	static const double fraction = (1.0 / static_cast<double>(RAND_MAX));
 
 	//std::cout << "\n Fraction is: " << fraction << "\n";
 	int random1 = rand();
 	//std::cout << "Random numberis: " << random1 << "\n";
 
-	double scaledRandom = (random1 * s_fraction);
+	double scaledRandom = (random1 * fraction);
 	//std::cout << "Random scaled numberis: " << scaledRandom << "\n";
 	
 	double rangedRandom = (scaledRandom * max );
@@ -82,149 +81,28 @@ int GetRandomNumber(int min, int max)
 
 	return static_cast<int>(randomfullyRanged);
 }
-void SetSeedBasedOnTime()
+void initializeRandom()
 {//set seed for prng to system time via time C functon
 	srand(static_cast<unsigned int>(time(0)));
 }
-void PrintRandomNumbers(int amount)
+void printRandonNumbers(int amount)
 {
 	for ( int count = 1; count <= amount; count++)
 	{
-		std::cout << GetRandomNumber(1,10) << " ";
+		std::cout << getRandomNumber(1,10) << " ";
 
 		if (count % 5 == 0)
-		{ 
 			std::cout << "\n";
-		}
+
 	}
 
 }
-bool WantToRepeatHilogame()
-{
-	//repeats hilogame if user wants to 
-	using std::cout;
-	using std::cin;
-
-
-	char replay;
-
-	//handle user input
-	while (true)
-	{
-		cout << "Would you like to play again? (y/n): ";
-		cin >> replay;
-		cin.ignore(32767, '\n');
-
-		if (replay == 'y' || replay == 'n')
-		{
-			break;
-		}
-		else
-		{
-			cout << "Invalid Input - try again \n";
-			continue;
-		}
-	}
-	//handle whether to play again
-	if (replay == 'y')
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
-}
-bool WonHiloGameLoopSimple()
-{
-	using std::cout;
-	using std::cin;
-	short userInput;
-
-	//first pngr call is always the same
-	GetRandomNumber(1, 100);
-
-	int randomNumber = GetRandomNumber(1, 100);
-
-	for (short attempts = 0; attempts <= 7; ++attempts)
-	{
-		cout << "Your Guess #" << attempts << ": ";
-		userInput = static_cast<int>(GetDoubleFromUser());
-
-
-
-
-		if (userInput == randomNumber)
-		{
-			cout << "You win. The number is indeed: " << randomNumber << ", just like your guess: " << userInput << "\n";
-			return true;
-		}
-
-		if (userInput > randomNumber)
-		{
-			cout << "The number you entered is too big. \n";
-		}
-
-		if (userInput < randomNumber)
-		{
-			cout << "The number you entered is too small. \n";
-		}
-	}
-	cout << "You lose. The number was: " << randomNumber << "\n";
-	return false;
-}
-void HiloGameShell()
-{
-	using std::cout;
-	using std::cin;
-	//	srand(static_cast<unsigned int>(time(0)));
-	//this needs to go into main()
-
-	while (true)
-	{
-		cout << "HINT: 	srand needs to run in main() \n";
-		//start initial game loop
-		//get back result of game (as int of num of attemptsleft, -1 counts as lost ;D)
-		bool hasWon = WonHiloGameLoopSimple();
-
-		if (hasWon)
-		{
-			cout << "You won the game. Congratulations. \n";
-		}
-
-		else
-		{
-			cout << "You lost the game. Too bad. \n";
-		}
-		//call interprete Game result function and print resulst
-
-		//ask if user wants to replay, in that case
-		bool willRepeat = WantToRepeatHilogame();
-
-		if (willRepeat)
-		{
-			cout << "Get ready for another round! \n";
-		}
-
-		else
-		{
-			cout << "Thanks for playing! \n";
-			break;
-		}
-	}
-}
-
-
-
-
-//forward declarations
-int  TestFunction();
-
 int main()
 {	
+
 	
-
-
+	
+	simpleCalculator();
 
 
 

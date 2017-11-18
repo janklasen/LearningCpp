fc93f0cba8daec0c309980f34b9b2de783bc7b6b
janklasen <33368027+janklasen@users.noreply.github.com>
2017-11-18 01:30:08 +0100

    Error handling if memory allocation fails

--- CiaoWorld/MathFunctions.cpp
+++ CiaoWorld/MathFunctions.cpp
@@ -322,13 +322,19 @@ void				Create1DArrayFromUserInput()
 	length = GetIntInRangeFromUser(0, 2147483647);
 	std::cout << "the length of your array is: " << length << "\n";
 
-	int *array = new int[length];
+	int *array = new (std::nothrow) int[length];
+	//handle errors
+	if (!array)
+	{
+		std::cout << "ERROR: Create1DArrayFromUserInput() could not allocate memory\n";
+		return;
+	}
 
-		char input;
-		std::cout << "Enter any letter to delete the array: ";
-		std::cin >> input;
-		std::cin.clear();
-		std::cin.ignore(32767, '\n');
+	char input;
+	std::cout << "Enter any letter to delete the array: ";
+	std::cin >> input;
+	std::cin.clear();
+	std::cin.ignore(32767, '\n');
 
 	delete[] array;
 	array = nullptr;
